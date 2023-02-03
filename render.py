9266c7ea69a1e6a38a3d669ee7b83bac9b419f4a
Jonathan Hellwig <jonathan.hellwig@posteo.de>
2023-02-03 23:30:24 +0100

    Update render function

--- render.py
+++ render.py
@@ -2,7 +2,8 @@ from typing import List
 import matplotlib.pyplot as plt
 
 
-def render(reflectors, rays, xlim=[0,100], zlim=[0,100]):
+def render(reflectors, rays, xlim=[0, 100], zlim=[0, 100]):
+    plt.rcParams['figure.figsize'] = [10, 10]
     for reflector in reflectors:
         ax = plt.gca()
         ax.set_xlim(xlim)
@@ -10,8 +11,9 @@ def render(reflectors, rays, xlim=[0,100], zlim=[0,100]):
         ax.invert_yaxis()
         plt.plot([reflector.start[0], reflector.end[0]], [
                  reflector.start[1], reflector.end[1]], color='r')
-    for ray in rays:
+    for i, ray in enumerate(rays):
         if len(ray.position_history) > 1:
             for first_position, second_position in zip(ray.position_history[:-1], ray.position_history[1:]):
                 plt.plot([first_position[0], second_position[0]], [first_position[1], second_position[1]],
-                         '-*', color='blue')
+                         '-*', label=f'Ray {i}')
+    plt.legend()

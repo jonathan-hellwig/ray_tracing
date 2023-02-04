b60789c2aef2a324358c25448b38607ca90c63ad
Jonathan Hellwig <jonathan.hellwig@posteo.de>
2023-02-04 11:08:30 +0100

    Improve rendering

--- render.py
+++ render.py
@@ -5,15 +5,20 @@ import matplotlib.pyplot as plt
 def render(reflectors, rays, xlim=[0, 100], zlim=[0, 100]):
     plt.rcParams['figure.figsize'] = [10, 10]
     for reflector in reflectors:
-        ax = plt.gca()
-        ax.set_xlim(xlim)
-        ax.set_ylim(zlim)
-        ax.invert_yaxis()
         plt.plot([reflector.start[0], reflector.end[0]], [
                  reflector.start[1], reflector.end[1]], color='r')
     for i, ray in enumerate(rays):
         if len(ray.position_history) > 1:
-            for first_position, second_position in zip(ray.position_history[:-1], ray.position_history[1:]):
-                plt.plot([first_position[0], second_position[0]], [first_position[1], second_position[1]],
-                         '-*', label=f'Ray {i}')
+            for j, (first_position, second_position) in enumerate(zip(ray.position_history[:-1], ray.position_history[1:])):
+                if j == 0:
+                    p = plt.plot([first_position[0], second_position[0]], [first_position[1], second_position[1]],
+                                 '-*', label=f'Ray {i}')
+                else:
+                    plt.plot([first_position[0], second_position[0]], [first_position[1], second_position[1]],
+                             '-*', c=p[0].get_c())
+
+    ax = plt.gca()
+    ax.set_xlim(xlim)
+    ax.set_ylim(zlim)
+    ax.invert_yaxis()
     plt.legend()
